6f9d9ee9c5549ec1f61feb2981b07418be6f012c
Davina Taylor <davinataylor@Davinas-MacBook-Air.local>
2015-03-25 19:53:18 -0700

    refactor

--- my_project.py
+++ my_project.py
@@ -7,46 +7,32 @@ def my_project():
 		computer_choice = random.choice(objects)
 		print computer_choice
 		player1_choice = raw_input('What do you want to play?')
-		if computer_choice == 'rock' and player1_choice == 'paper':
-			print "player1 will beat the computer"
-		if computer_choice == 'paper' and player1_choice == 'scissors':
-			print "player1 will beat the computer"
-		if computer_choice == 'scissors' and  player1_choice == 'rock':
-			print "player1 will beat the computer"
-		if computer_choice == 'paper' and player1_choice == 'paper':
-			print "It will be a tie"
-		if computer_choice == 'rock' and player1_choice == 'scissors':
-			print "the computer will beat player1"
-		if computer_choice == 'rock' and player1_choice == 'rock':
-			print "It will be a tie"
-		if computer_choice == 'paper' and player1_choice == 'rock':
-			print "the computer will beat player1"
-		if computer_choice == 'scissors' and player1_choice == 'paper':
-			print "the computer will beat player1"
-		if computer_choice == 'scissors' and player1_choice == 'scissors':
-			print "It will be a tie"						
-	# computer_choice = random.choice(objects)
-	# print player1_choice
-	# player2_choice = raw_input('What do you want to play?')
-	# if player1_choice == 'rock' and player2_choice == 'paper':
-	# 	print "player2 will beat player1"
-	# if player1_choice == 'paper' and player2_choice == 'scissors':
-	# 	print "player2 will beat player1"
-	# if player1_choice == 'scissors' and player2_choice == 'rock':
-	# 	print "player2 will beat player1"
-	# if player1_choice == 'paper' and player2_choice == 'paper':
-	# 	print "It will be a tie"
-	# if player1_choice == 'rock' and player2_choice == 'scissors':
-	# 	print "player1 will beat player2"
-	# if player1_choice == 'rock' and player2_choice == 'rock':
-	# 	print "It will be a tie"	
-	# if player1_choice == 'paper' and player2_choice == 'rock':
-	# 	print "player1 will beat player2"
-	# if player1_choice == 'scissors' and player2_choice == 'paper':
-	# 	print "player1 will beat player2"
-	# if player1_choice == 'scissors' and player2_choice == 'scissors':
-	# 	print "It will be a tie"								
-	
+		my_comparisons(computer_choice, player1_choice)
+def my_comparisons(computer_choice, player1_choice):
+	if(computer_choice == 'rock' and player1_choice == 'paper' or computer_choice == 'paper' and player1_choice == 'scissors' or computer_choice == 'scissors' and player1_choice == 'rock'):
+		print "player1 will beat the computer"
+	if(computer_choice == 'rock' and player1_choice == 'scissors' or computer_choice == 'paper' and player1_choice == 'rock' or computer_choice == 'scissors' and player1_choice == 'paper'):
+		print "the computer will beat player1"
+	if(computer_choice == 'paper' and player1_choice == 'paper' or computer_choice == 'rock' and player1_choice == 'rock' or computer_choice == 'scissors' and player1_choice == 'scissors'):
+		print "It will be a tie"
+		# if computer_choice == 'rock' and player1_choice == 'paper':
+		# 	print "player1 will beat the computer"
+		# if computer_choice == 'paper' and player1_choice == 'scissors':
+		# 	print "player1 will beat the computer"
+		# if computer_choice == 'scissors' and  player1_choice == 'rock':
+		# 	print "player1 will beat the computer"
+		# if computer_choice == 'paper' and player1_choice == 'paper':
+		# 	print "It will be a tie"
+		# if computer_choice == 'rock' and player1_choice == 'scissors':
+		# 	print "the computer will beat player1"
+		# if computer_choice == 'rock' and player1_choice == 'rock':
+		# 	print "It will be a tie"
+		# if computer_choice == 'paper' and player1_choice == 'rock':
+		# 	print "the computer will beat player1"
+		# if computer_choice == 'scissors' and player1_choice == 'paper':
+		# 	print "the computer will beat player1"
+		# if computer_choice == 'scissors' and player1_choice == 'scissors':
+		# 	print "It will be a tie"														
 my_project()
 
  		
